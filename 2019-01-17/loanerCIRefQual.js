0847f017c03bff5e6cce7503cfb7a01d7d2a4b05
Chuck Tomasi <chuck.tomasi@gmail.com>
2019-01-17 07:11:09 -0700

    2019-01-17 script

---
+++ 2019-01-17/loanerCIRefQual.js
@@ -0,0 +1,34 @@
+/*   This is an advanced qualifier function for the cmdb_ci field
+ *   To use it, add the following line to the CI ref qualifier
+ *	javascript:loanerCIRefQual()
+ */
+
+function loanerCIRefQual() {
+	
+	gs.info('>>>loanerCIRefQual(): start');
+	var answer;
+	var req = {};
+	
+	if (current.variables.item_type) {
+		gs.info(">>>CATALOG ITEM");
+		req.depot = current.variables.depot.toString();
+		req.item_type = current.variables.item_type.toString();
+		req.work_start = current.variables.work_start;
+		req.work_end = current.variables.work_end;
+		
+	} else {
+		gs.info('>>>FORM');
+		req.depot = current.depot;
+		req.item_type = current.item_type;
+		req.work_start = current.work_start;
+		req.work_end = current.work_end;
+		
+	}
+	
+	gs.info('>>>loanerCIRefQual(): ' + JSON.stringify(req, null, 4));
+	
+	var lu = new LoanerUtils();
+	answer = 'sys_idIN' + lu.availableCis(req.item_type, req.depot, req.work_start, req.work_end);
+	
+	return answer;
+}
